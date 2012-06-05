a186bb899f3e701359e5b289f63ba524169fe1ae
Brandon Bickford <bickfordb@gmail.com>
2012-06-05 04:55:41 -0700

    forgot this one

---
+++ src/peloton/bits.clj
@@ -0,0 +1,91 @@
+(ns peloton.bits
+  (:import java.io.ByteArrayInputStream)
+  (:import java.io.ByteArrayOutputStream)
+  (:import java.nio.ByteBuffer)
+  (:import java.nio.charset.Charset)
+  (:use peloton.util))
+
+(defn put-le-i32! 
+  [^ByteArrayOutputStream bs
+   ^long n]
+  (.write bs (bit-and 0x0ff n))
+  (.write bs (bit-and 0x0ff (bit-shift-right n 8)))
+  (.write bs (bit-and 0x0ff (bit-shift-right n 16)))
+  (.write bs (bit-and 0x0ff (bit-shift-right n 24))))
+
+(defn put-le-i64! 
+  [^ByteArrayOutputStream bs
+   ^long n]
+  (.write bs (bit-and 0x0ff n))
+  (.write bs (bit-and 0x0ff (bit-shift-right n 8)))
+  (.write bs (bit-and 0x0ff (bit-shift-right n 16)))
+  (.write bs (bit-and 0x0ff (bit-shift-right n 24)))
+  (.write bs (bit-and 0x0ff (bit-shift-right n 32)))
+  (.write bs (bit-and 0x0ff (bit-shift-right n 40)))
+  (.write bs (bit-and 0x0ff (bit-shift-right n 48)))
+  (.write bs (bit-and 0x0ff (bit-shift-right n 56))))
+
+(defn put-cstring!
+  [^ByteArrayOutputStream bs
+   ^String key]
+  (let [key0 (.getBytes key #^Charset UTF-8)]
+    (.write bs key0 0 (count key0)))
+  (.write bs 0))
+
+(defn read-le-i32!
+  ^long
+  [^ByteArrayInputStream bs]
+  (if (>= (.available bs) 4)
+    (bit-or (.read bs) 
+            (bit-shift-left (.read bs) 8)
+            (bit-shift-left (.read bs) 16)
+            (bit-shift-left (.read bs) 24)) 0))
+
+(defn read-le-i64! 
+  ^long
+  [^ByteArrayInputStream bs]
+  (if (>= (.available bs) 8)
+    (bit-or (.read bs) 
+            (bit-shift-left (.read bs) 8)
+            (bit-shift-left (.read bs) 16)
+            (bit-shift-left (.read bs) 24) 
+            (bit-shift-left (.read bs) 32)
+            (bit-shift-left (.read bs) 40)
+            (bit-shift-left (.read bs) 48)
+            (bit-shift-left (.read bs) 56)) 0))
+
+(defn read-i8! 
+  ^Integer
+  [^ByteArrayInputStream bs]
+  (when (>= (.available bs) 1)
+    (.read bs)))
+
+(defn read-cstring! 
+  ^String
+  [^ByteArrayInputStream bs]
+  (let [buf (ByteArrayOutputStream. 1)]
+    (loop []
+      (let [b (read-i8! bs)]
+        (cond 
+          (nil? b) nil
+          (= b 0x0) (String. (.toByteArray buf) #^Charset UTF-8)
+          :else (let []
+                  (.write buf b)
+                  (recur)))))))
+
+(defn slice-buffer
+  ^bytes [^ByteBuffer b
+   offset
+   len]
+  (java.util.Arrays/copyOfRange (.array b) (long offset) (long (+ offset len))))
+
+(defn slice-remaining-buffer
+  ^bytes [^ByteBuffer b]
+  (with-stderr
+    (println b))
+  (slice-buffer b (.position b) (long (- (.limit b) (.position b)))))
+
+(defn remove-bit
+  [bits n]
+  (bit-and bits (bit-flip n)))
+
