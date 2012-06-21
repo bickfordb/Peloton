61f449de73c2570a3d110254e817160c3f5d1234
Brandon Bickford <bickfordb@gmail.com>
2012-06-20 17:04:03 -0700

    update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject peloton "0.1.1"
-            :description "NIO based web library"
+            :description "Peloton is a Clojure library to simplify the creation of high performance event-based web applications."
             :url "http://github.com/bickfordb/Peloton"
             :dependencies [[hiccup "1.0.0"]
                            [org.clojure/tools.logging "0.2.3"]
